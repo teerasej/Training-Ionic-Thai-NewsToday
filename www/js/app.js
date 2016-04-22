d052c07a2f0b4317e7b6fd98944e256079ccb8b1
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-04-22 22:54:29 +0700

    Implement  call to test.php to check server is work

--- www/js/app.js
+++ www/js/app.js
@@ -6,30 +6,36 @@
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    $ionicPlatform.ready(function() {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
 
-.controller('NewsController', ['$scope','$http', function($scope, $http){
-  
-  $scope.news = [];
+.controller('NewsController', ['$scope', '$http', function($scope, $http) {
 
-  $scope.loadNews = function(){
+    $scope.news = [];
 
-  }
+    $scope.loadNews = function() {
+        $http.get('localhost/test.php')
+            .success(function() {
+                console.log('Oh, yeah!');
+            })
+            .error(function() {
+                console.log('Error.');
+            })
+    }
 
 
 }])
