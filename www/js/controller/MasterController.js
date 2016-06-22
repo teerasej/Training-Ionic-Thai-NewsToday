7f5fc8ff7ac9750ef23daa918e75051236a04fef
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-06-22 16:50:25 +0700

    Implement call all news item from Web API

--- www/js/controller/MasterController.js
+++ www/js/controller/MasterController.js
@@ -3,15 +3,18 @@ app.controller('MasterController', ['$scope', '$state','NewsService', function($
     $scope.news = [];
 
     $scope.loadNews = function() {
-        var mockupNews = [];
-        $scope.news = mockupNews;
+        NewsService.loadNews($scope.updateNews);
+    }
+
+    $scope.updateNews = function(news){
+        console.log('Hooray! news from web api is here.');
+        $scope.news = news;
     }
 
     $scope.showDetail = function(newsItem){
         NewsService.selectedNews = newsItem;
         $state.go('detail');
     }
-
 
     $scope.loadNews();
 
--- www/js/service/NewsService.js
+++ www/js/service/NewsService.js
@@ -1,6 +1,29 @@
-app.service('NewsService', function(){
+app.service('NewsService', function($http){
 
 	return {
-		selectedNews: {}
+
+		url: 'http://localhost:8888/news/',
+
+		selectedNews: {},
+
+		loadNews : function(callBackFunction, count){
+
+			console.log('Going to call Web API');
+
+			if(count && count > 0){
+				console.info('Not implemented');
+			} else {
+				console.info('Accessing ' + this.url);
+
+				$http.get(this.url)
+					.success(function(data){
+						console.dir(data);
+						callBackFunction(data);
+					})
+					.error(function(error){
+						console.error('Error');
+					})
+			}
+		}
 	};
 })
