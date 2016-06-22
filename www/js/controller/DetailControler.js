13f44390926bb90c0e11d5b1d2afab1cfe41c89c
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-06-22 16:20:11 +0700

    Assign NewsService's object to show in scope's object

--- www/js/controller/DetailControler.js
+++ www/js/controller/DetailControler.js
@@ -1,8 +1,7 @@
 app.controller('DetailController', ['$scope','$stateParams', 'NewsService' , function($scope, $stateParams, NewsService) {
 
 
-    // $scope.newsItem = NewsService.selectedNews;
-    var title = $stateParams.title;
-    $scope.title = title;
+    $scope.newsItem = NewsService.selectedNews;
+ 
 
 }])
