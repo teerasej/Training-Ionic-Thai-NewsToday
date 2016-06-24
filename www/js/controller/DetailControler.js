9d547faaff5af9b5e16e371daeb521630ad1a5bf
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-06-24 16:36:03 +0700

    Adjust empty line

--- www/js/controller/DetailControler.js
+++ www/js/controller/DetailControler.js
@@ -1,7 +1,5 @@
 app.controller('DetailController', ['$scope','$stateParams', 'NewsService' , function($scope, $stateParams, NewsService) {
 
-
     $scope.newsItem = NewsService.selectedNews;
- 
 
 }])
