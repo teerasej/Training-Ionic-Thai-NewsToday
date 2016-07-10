cceec91e3f3115c0c446309f4bcb96fe6952cd73
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-07-10 23:06:17 +0700

    Use NewsService to pass username & password to web service

--- www/js/controller/UserController.js
+++ www/js/controller/UserController.js
@@ -1,7 +1,15 @@
-app.controller('UserController',['$scope', function($scope){
+app.controller('UserController',['$scope','NewsService', function($scope, NewsService){
 
   $scope.signIn = function(username, password){
+      NewsService.signIn(username,password)
+        .success(function(data){
 
+          console.dir(data);
+
+        })
+        .error(function(error){
+          console.error('Error');
+        })
   }
 
 
